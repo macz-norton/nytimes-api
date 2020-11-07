c0ddd7aa73a9c058fb54465a255249775c5631e7
Danny Maier <dannymaier@gmail.com>
2020-11-07 13:12:47 -0800

    added Dom elements and global variables for input fields

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 //DECLARE DOM objects
-
+var searchTermEl = $("#searchTerm")
+    console.log(searchTermEl)
+var numberRecordsEl = $("#numberRecords")
+    console.log(numberRecordsEl)
+var startYearEl = $("#startYear")
+    console.log(startYearEl)
+var endYearEl = $("#endYear")
+    console.log(endYearEl)
 //DECLARE global variables
+var searchTermElVal = searchTermEl.val()
+    console.log(searchTermElVal)
+var numberRecordsElVal = numberRecordsEl.val()
+    console.log(numberRecordsElVal)
+var startYearElVal = startYearEl.val()
+    console.log(startYearElVal)
+var endYearElVal = endYearEl.val()
+    console.log(endYearElVal)
 
 
 var APIKey = "";
@@ -10,9 +25,9 @@ var queryURL = "" + APIKey;
 
 
 // We then created an AJAX call
-$.ajax({
-url: queryURL,
-method: "GET"
-}).then(function(response) {
-console.log(response)
-}
+// $.ajax({
+// url: queryURL,
+// method: "GET"
+// }).then(function(response) {
+// console.log(response)
+// }
